8890518920fbdf2131e509935162fa4a2359a5a9
Maryna Zhukovich <marina.x.sea@gmail.com>
2022-05-16 10:06:53 +0300

    add: adaptive (Mon, May 16,2022 10:06:32 AM)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -40,28 +40,23 @@ createControllers()
 
 function createCards() {
   sliderElementWrapper.innerHTML = "";
-
   mems.forEach((el, index) => {
     console.log(el.id)
-    sliderElement =
-      `
+    sliderElement = `
       <img class="slider-img" src="${el.img}" alt=${el.id} />              
       `
-
     if (index == 0) {
-      sliderElementText =
-        `      
+      sliderElementText = `      
       <div class="slider-text" style="visibility:visible">
         <span class="slider-text-t">${el.text}</span>
       </div>               
-   `
+      `
     } else {
-      sliderElementText =
-    `
+      sliderElementText = `
       <div class="slider-text">
         <span class="slider-text-t">${el.text}</span>
       </div>               
-    `
+      `
     }
     sliderElementWrapper.innerHTML += sliderElement
     sliderTextWrapper.innerHTML += sliderElementText
@@ -87,23 +82,15 @@ let controllersBtnsInner = document.querySelectorAll(".controller-inner")
 let sliderElements = document.querySelectorAll(".slider-elem")
 controllersBtnsInner[0].classList.add('controller-inner-active')
 
-function changeOrder(index) {
-  sliderElements.forEach((slide) => {
-    slide.classList.remove('order')
-  })
-}
-
 controllersBtns.forEach((el, index) => {
   el.addEventListener("click", () => {
     sliderText.forEach((el) => {
       el.classList.remove('tooltip')
     })
-
     controllersBtnsInner.forEach((el) => {
       el.classList.remove("controller-inner-active")
-      })
+    })
     controllersBtnsInner[index].classList.add('controller-inner-active')
-
     el.classList.remove("controller-inner-active")
     sliderWrapper.classList.remove('transition-right-1-left')
     sliderWrapper.classList.remove('transition-right-left')
@@ -116,93 +103,69 @@ controllersBtns.forEach((el, index) => {
       sliderWrapper.classList.add('transition-right-1')
       sliderWrapper.classList.add('transition-right-1-left')
       sliderTextWrapperAnimation.classList.add('transition-left-text')
-      // sliderWrapper.style.left = '0'
       sliderTextWrapperAnimation.style.left = '0'
     } else if (index == 0 && indexOfPreviewSlide == 2) {
       sliderWrapper.classList.add('transition-right-2')
       sliderWrapper.classList.add('transition-right-1-left')
       sliderTextWrapperAnimation.classList.add('transition-left-text')
-      // sliderWrapper.style.left = '0'
       sliderTextWrapperAnimation.style.left = '0'
-    }
-    else if (index == 0 && indexOfPreviewSlide == 3) {
+    } else if (index == 0 && indexOfPreviewSlide == 3) {
       sliderWrapper.classList.add('transition-right-3')
       sliderWrapper.classList.add('transition-right-1-left')
       sliderTextWrapperAnimation.classList.add('transition-left-text')
-      // sliderWrapper.style.left = '0'
       sliderTextWrapperAnimation.style.left = '0'
-    }
-
-
-
-    else if (index == 1 && indexOfPreviewSlide < 1) {
+    } else if (index == 1 && indexOfPreviewSlide < 1) {
       sliderWrapper.classList.add('transition-right')
       sliderWrapper.classList.add('transition-right-left')
       sliderTextWrapperAnimation.classList.add('transition-right-text-25')
-      // sliderWrapper.style.left = '-50.5em'
       sliderTextWrapperAnimation.style.left = '-22em'
-    }
-    else if (index == 1 && indexOfPreviewSlide == 2) {
+    } else if (index == 1 && indexOfPreviewSlide == 2) {
       sliderWrapper.classList.add('transition-right-2-3')
       sliderWrapper.classList.add('transition-right-left')
-      // sliderTextWrapperAnimation.classList.add('transition-right-text-25')
       sliderTextWrapperAnimation.classList.add('transition-move-left-text-3-2')
-      // sliderWrapper.style.left = '-50.5em'
       sliderTextWrapperAnimation.style.left = '-22em'
     } else if (index == 1 && indexOfPreviewSlide == 3) {
       sliderWrapper.classList.add('transition-right-2-4')
       sliderWrapper.classList.add('transition-right-left')
-      // sliderTextWrapperAnimation.classList.add('transition-right-text-25')
       sliderTextWrapperAnimation.classList.add('transition-move-left-text-1-3')
-      // sliderWrapper.style.left = '-50.5em'
       sliderTextWrapperAnimation.style.left = '-22em'
     } else if (index == 2 && indexOfPreviewSlide == 1) {
       sliderWrapper.classList.add('transition-right-104')
       sliderWrapper.classList.add('transition-right-104-left')
       sliderTextWrapperAnimation.classList.add('transition-right-text-50')
-      // sliderWrapper.style.left = '-102em'
       sliderTextWrapperAnimation.style.left = '-44em'
-    }
-    else if (index == 2 && indexOfPreviewSlide == 0) {
+    } else if (index == 2 && indexOfPreviewSlide == 0) {
       sliderWrapper.classList.add('transition-right-3-1')
       sliderWrapper.classList.add('transition-right-104-left')
       sliderTextWrapperAnimation.classList.add('transition-right-text-50')
-      // sliderWrapper.style.left = '-102em'
       sliderTextWrapperAnimation.style.left = '-44em'
     } else if (index == 2 && indexOfPreviewSlide == 3) {
       sliderWrapper.classList.add('transition-right-3-4')
       sliderWrapper.classList.add('transition-right-104-left')
-      // sliderTextWrapperAnimation.classList.add('transition-right-text-50')
       sliderTextWrapperAnimation.classList.add('transition-move-left-text-2-3')
-      // sliderWrapper.style.left = '-102em'
       sliderTextWrapperAnimation.style.left = '-44em'
-    }
-    else if (index == 3 && indexOfPreviewSlide == 2) {
+    } else if (index == 3 && indexOfPreviewSlide == 2) {
       sliderWrapper.classList.add('transition-right-156')
       sliderWrapper.classList.add('transition-right-156-left')
       sliderTextWrapperAnimation.classList.add('transition-right-text-75')
-      // sliderWrapper.style.left = '-153.5em'
       sliderTextWrapperAnimation.style.left = '-66em'
-    }
-    else if (index == 3 && indexOfPreviewSlide == 1) {
+    } else if (index == 3 && indexOfPreviewSlide == 1) {
       sliderWrapper.classList.add('transition-right-4-1')
       sliderWrapper.classList.add('transition-right-156-left')
       sliderTextWrapperAnimation.classList.add('transition-right-text-75')
-      // sliderWrapper.style.left = '-153.5em'
       sliderTextWrapperAnimation.style.left = '-66em'
-    }
-    else if (index == 3 && indexOfPreviewSlide == 0) {
+    } else if (index == 3 && indexOfPreviewSlide == 0) {
       sliderWrapper.classList.add('transition-right-4-0')
       sliderWrapper.classList.add('transition-right-156-left')
       sliderTextWrapperAnimation.classList.add('transition-right-text-75')
-      // sliderWrapper.style.left = '-153.5em'
       sliderTextWrapperAnimation.style.left = '-66em'
+    } else if (index == indexOfPreviewSlide) {
+      controllersBtnsInner[index].classList.add('controller-inner-active')
     }
   })
 })
 
 sliderTextWrapperAnimation.addEventListener('animationstart', () => {
-
   sliderText.forEach((el) => {
     el.classList.add('tooltip1')
   })
